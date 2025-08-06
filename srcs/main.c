1da5498535556c1f72b56192a976c00514053741
Tazokami <taz.okami@hotmail.com>
2025-08-06 23:19:59 +0200

    clean

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,19 @@
 /*   By: Jpaulis <Jpaulis@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 15:33:44 by Jpaulis           #+#    #+#             */
-/*   Updated: 2025/08/06 15:47:48 by Jpaulis          ###   ########.fr       */
+/*   Updated: 2025/08/06 21:46:05 by Jpaulis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+static int	error_msg(char *message)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(message, 2);
+	ft_putstr_fd("\n", 2);
+	return (1);
+}
 
 static int	game_loop(t_game *game)
 {
@@ -22,34 +30,21 @@ static int	game_loop(t_game *game)
 		game->mlx.image_ptr, 0, 0);
 	return (0);
 }
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
 
 	if (argc != 2)
-	{
-		ft_putstr_fd("Error\n", 2);
-		ft_putstr_fd("Usage: ./cub3d <map.cub>\n", 2);
-		return (1);
-	}
-
-	// Vérifier l'extension avant d'initialiser quoi que ce soit
+		return (error_msg("Usage: ./cub3d <map.cub>\n"));
 	if (!argv[1] || !ft_strstr(argv[1], ".cub"))
-	{
-		ft_putstr_fd("Error\n", 2);
-		ft_putstr_fd("File must have .cub extension\n", 2);
-		return (1);
-	}
-
+		return (error_msg("File must have .cub extension"));
 	ft_memset(&game, 0, sizeof(t_game));
 	init_game_defaults(&game);
 	if (init_mlx(&game))
 		cleanup_and_exit(&game, "MLX initialization failed");
 	if (load_map(argv[1], &game))
-	{
-		cleanup_game(&game);
-		return (1);
-	}
+		cleanup_and_exit(&game, "Map loading failed");
 	if (load_textures(&game))
 		cleanup_and_exit(&game, "Texture loading failed");
 	mlx_hook(game.mlx.window_ptr, 2, 1L << 0, handle_keypress, &game);
@@ -57,8 +52,6 @@ int	main(int argc, char **argv)
 	mlx_hook(game.mlx.window_ptr, 17, 0, handle_destroy, &game);
 	mlx_loop_hook(game.mlx.mlx_ptr, game_loop, &game);
 	mlx_loop(game.mlx.mlx_ptr);
-	printf("🔄 Cleaning...\n");
 	cleanup_game(&game);
 	return (0);
 }
-
